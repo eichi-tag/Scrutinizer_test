95a1edb4d0536b1794e27a961e95baf4fdd1a88f
e-taguchi <ei-taguchi@tv-bank.co.jp>
2015-12-10 17:15:00 +0900

    2nd

--- RedisManager/config.inc.php
+++ RedisManager/config.inc.php
@@ -1,12 +1,12 @@
 [redis]
-prod.new_host[]=192.168.10.142:6379
-prod.new_host[]=192.168.10.142:6380
+prod.new_host[]=192.0.0.1:6379
+prod.new_host[]=192.0.0.1:6380
 
-test.new_host[]=192.168.10.142:6380
+test.new_host[]=192.0.0.1:6380
 
-prod.prev_host[]=192.168.10.142:6379
-prod.prev_host[]=192.168.10.142:6380
+prod.prev_host[]=192.0.00.1:6379
+prod.prev_host[]=192.0.0.1:6380
 
-test.prev_host[]=192.168.10.142:6380
+test.prev_host[]=192.0.0.1:6380
 
 expire = 60
